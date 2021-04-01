d48a728e7ffa9751f7a24965fb06d75ae6fe2cbd
Alexander Chambers <fainspirit@gmail.com>
2021-04-01 01:16:50 -0500

    Initial Commit

---
+++ LegendsOfRuneterraHelper/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading;
+
+namespace LegendsOfRuneterraHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RuneterraAPIHelper apiHelper = RuneterraAPIHelper.GetInstance();
+            Console.WriteLine("Trying to connect to game server at {0}", apiHelper.GetEndpointString());
+
+            bool wantToExit = false;
+
+            while (!wantToExit)
+            {
+                // Try to find a running instance of the game
+                while (!apiHelper.APIAvailable())
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Could not connect... Retrying in 1 second");
+                }
+                Console.WriteLine("Connected!");
+                Console.WriteLine();
+
+                JsonDocument deck;
+                JsonDocument expedition;
+                JsonDocument gameResult;
+                JsonDocument positionalRectangles;
+
+
+                JsonElement deckRoot;
+                JsonElement expeditionRoot;
+                JsonElement gameResultRoot;
+                JsonElement positionalRectanglesRoot;
+
+                JsonElement Rectangles;
+
+
+                float delay = 5.0f;
+
+                // Continually query and output the API state for now
+                // Sends a query every delay seconds
+                while (apiHelper.APIAvailable())
+                {
+                    apiHelper.QueryDeck(out deck);
+                    apiHelper.QueryExpeditionState(out expedition);
+                    apiHelper.QueryGameResult(out gameResult);
+                    apiHelper.QueryPositionalRectangles(out positionalRectangles);
+
+                    deckRoot = deck.RootElement;
+                    expeditionRoot = expedition.RootElement;
+                    gameResultRoot = gameResult.RootElement;
+                    positionalRectanglesRoot = positionalRectangles.RootElement;
+
+                    JsonSerializerOptions o = new JsonSerializerOptions();
+                    o.WriteIndented = true;
+
+                    if (positionalRectanglesRoot.TryGetProperty("Rectangles", out Rectangles))
+                    {
+                        Console.WriteLine(Rectangles);
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("Deck: " + deckRoot);
+                    Console.WriteLine();
+
+                    //Console.WriteLine("Expedition: " + expeditionRoot);
+                    //Console.WriteLine("Last Game Result: " + gameResultRoot);
+                    //Console.WriteLine("Positional Rectangles: " + positionalRectanglesRoot);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    // Sleep for delay seconds
+                    Thread.Sleep(1000 * 5);
+                }
+            }
+        }
+    }
+}
---
+++ LegendsOfRuneterraHelper/RuneterraAPIHelper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json;
+namespace LegendsOfRuneterraHelper
+{
+    public class RuneterraAPIHelper
+    {
+        static RuneterraAPIHelper instance;
+
+        static readonly string STATIC_DECKLIST_ENDPOINT = "static-decklist";
+        static readonly string POSITIONAL_RECTANGLES_ENDPOINT = "positional-rectangles";
+        static readonly string EXPEDITION_STATUS_ENDPOINT = "expeditions-state";
+        static readonly string GAME_RESULT_ENDPOINT = "game-result";
+
+        static int DEFAULT_PORT = 21337;
+        static string DEFAULT_ADDRESS = "127.0.0.1";
+        static IPEndPoint localEndpoint;
+        static Ping ping;
+
+        /////////////////////
+        // PUBLIC METHODS //
+        /////////////////////
+        
+        public static RuneterraAPIHelper GetInstance()
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+            instance = new RuneterraAPIHelper();
+            return instance;
+
+        }
+
+        public bool APIAvailable()
+        {
+            JsonDocument temp;
+            return QueryGameResult(out temp);
+        }
+
+        public string GetEndpointString()
+        {
+            return localEndpoint.ToString();
+        }
+
+        public bool SetAddress(string newAddress)
+        {
+            bool success = IPEndPoint.TryParse(DEFAULT_ADDRESS + ":" + DEFAULT_PORT, out localEndpoint);
+            if (success)
+            {
+                return true;
+            }
+            Console.Error.WriteLine("Invalid Address");
+            return false;
+        }
+
+        public bool QueryDeck(out JsonDocument result)
+        {
+            return Query("decklist", out result);
+        }
+
+        public bool QueryPositionalRectangles(out JsonDocument result)
+        {
+            return Query("positions", out result);
+        }
+
+        public bool QueryExpeditionState(out JsonDocument result)
+        {
+            return Query("expedition", out result);
+        }
+
+        public bool QueryGameResult(out JsonDocument result)
+        {
+            return Query("gameresult", out result);
+        }
+
+        /////////////////////
+        // PRIVATE METHODS //
+        /////////////////////
+        
+        private bool Query(string endpoint, out JsonDocument result)
+        {
+            StringBuilder URLBuilder = new StringBuilder();
+            URLBuilder.Append("http://" + localEndpoint.ToString() + "/");
+
+            switch (endpoint) 
+            {
+                case "decklist":
+                    {
+                        URLBuilder.Append(STATIC_DECKLIST_ENDPOINT);
+                        break;
+                    }
+                case "positions":
+                    {
+                        URLBuilder.Append(POSITIONAL_RECTANGLES_ENDPOINT);
+                        break;
+                    }
+                case "expedition":
+                    {
+                        URLBuilder.Append(EXPEDITION_STATUS_ENDPOINT);
+                        break;
+                    }
+                case "gameresult":
+                    {
+                        URLBuilder.Append(GAME_RESULT_ENDPOINT);
+                        break;
+                    }
+            }
+
+            string URL = URLBuilder.ToString();
+            //Console.WriteLine("Querying {0}", URL);
+
+            // Save
+            WebRequest decklistRequest = WebRequest.CreateHttp(URL);
+            try
+            {
+                WebResponse response = decklistRequest.GetResponse();
+
+                System.IO.Stream dataStream = response.GetResponseStream();
+
+                result = JsonDocument.Parse(dataStream);
+                return true;
+            }
+            catch (System.Net.WebException e)
+            {
+                //Console.Error.WriteLine(e.Message);
+
+                result = null;
+                return false;
+            }
+        }
+
+        private RuneterraAPIHelper()
+        {
+            SetAddress(DEFAULT_ADDRESS + ":" + DEFAULT_PORT);
+            ping = new Ping();
+        }
+
+
+    }
+}
